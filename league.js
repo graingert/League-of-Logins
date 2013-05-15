ffd9018a343ac787d5a2202af69edce43ec14df5
Thomas Grainger <tagrain@gmail.com>
2013-05-15 01:26:22 +0100

    fix syntax errors

--- league.js
+++ league.js
@@ -1,19 +1,17 @@
 $(function(){
     "use strict";
-    function gravatar(email, size, rating, d){
+    function gravatar(email){
         return URI("http://www.gravatar.com/avatar/")
             .filename(email)
             .suffix("jpg")
             .query({
-                "s": size,
-                "r": rating,
-                "d": d
+                "s": 60
             }).toString();
     }
 
     var template_tag = $("#league-template");
     var template = Handlebars.compile(template_tag.html());
-    console.log(template_tag.html());
+
     $.ajax("users.json", {"dataType":"json", "mimeType":"application/json"}).done(function(data){
         template_tag.replaceWith(template(
             Enumerable.From(data)
@@ -24,7 +22,7 @@ $(function(){
                             "name" : item[1].gecos[0],
                             "login_count" : item[1].logonCount[0],
                             "ecsid": item[1].ecsEmail[0],
-                            "avatar" : gravatar(item[1].ecsEmail[0] + "@ecs.soton.ac.uk", size=60)
+                            "avatar" : gravatar(item[1].ecsEmail[0] + "@ecs.soton.ac.uk")
                         };
                     })
                     .ToArray()
